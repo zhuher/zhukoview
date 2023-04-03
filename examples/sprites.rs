234d301dd391450d7ba589d17f3118190aa5e34f
Herman Zhukov <societyofbruh@gmail.com>
2023-04-03 19:28:23 +0300

    Updated example, fixed dependencies

--- examples/sprites.rs
+++ examples/sprites.rs
@@ -1,24 +1,25 @@
-#[cfg(feature = "rng")]
+// #[cfg(all(feature = "rng", feature = "png"))]
 fn main() {
-    let paddingc = 0x808080FF;
-    let (spritew, spriteh) = (10u32, 7u32);
-    let (cols, rows) = (10, 10);
-    let padding = true;
-    let vsym = true;
-    let hsym = false;
-    let paddingw = 2;
+    let paddingc = 0xFF808080_u32; // padding colour in ARGB hex
+    let (spritew, spriteh) = (10_u32, 7_u32); // single sprite dimensions
+    let (cols, rows) = (4_u32, 3_u32); // amount of columns and rows of sprites
+    let padding = true; // to pad or not to pad that is the question...
+    let vsym = true; // vertical sprite symmetry (qp)
+    let hsym = false; // horisontal (qd)
+    let paddingw = 1; // padding width
     let cmap = [
-        0x1D2B53FF, 0x7E2553FF, 0x008751FF, 0xAB5236FF, 0x5F574FFF, 0xC2C3C7FF, 0xFFF1E8FF,
-        0xFF004DFF, 0xFFA300FF, 0xFFEC27FF, 0x00E436FF, 0x29ADFFFF, 0x83769CFF, 0xFF77A8FF,
-        0xFFCCAAFF,
-    ];
+        0xFF000000, 0xFF1D2B53, 0xFF7E2553, 0xFF008751, 0xFFAB5236, 0xFF5F574F, 0xFFC2C3C7,
+        0xFFFFF1E8, 0xFFFF004D, 0xFFFFA300, 0xFFFFEC27, 0xFF00E436, 0xFF29ADFF, 0xFF83769C,
+        0xFFFF77A8, 0xFFFFCCAA,
+    ]; // colour palette
     let totalw = spritew * cols + if padding { (cols + 1) * paddingw } else { 0 };
     let totalh = spriteh * rows + if padding { (rows + 1) * paddingw } else { 0 };
-    let mut c: zhucanvas::CanvasView =
-        zhucanvas::CanvasView::new(paddingc, totalw, totalh, zhucanvas::RGBType::RGBA32);
+    let view: zhukoview::CanvasView = zhukoview::CanvasView::new(totalw, totalh);
+    let mut canvas = vec![paddingc; (totalw * totalh) as usize];
     for row in 0..rows {
         for col in 0..cols {
-            c.add_sprite(
+            view.add_sprite(
+                &mut canvas,
                 (
                     (spritew * col + (u32::from(padding) + col * u32::from(padding)) * paddingw)
                         as isize,
@@ -33,8 +34,9 @@ fn main() {
             );
         }
     }
-    #[cfg(feature = "png")]
-    match pingus::create(totalw, totalh, &c.data, "img/spritemap100.png") {
+    let mut imgbuf = vec![0; (totalw * totalh) as usize];
+    view.argb32_to_rgba32(&canvas, &mut imgbuf);
+    match pingus::create(totalw, totalh, &imgbuf, "spritemap.png") {
         Ok(_) => println!("Spritemap created successfully."),
         Err(e) => println!("Failed to create da spritemap: {e}"),
     };
